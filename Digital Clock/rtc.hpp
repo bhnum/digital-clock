8b6b55d9a1133032e20e75bf7bc7081ee2442779
= <saedi.behnam.336@gmail.com>
2021-02-05 00:18:15 +0330

    Implemented RTC driver

--- Digital Clock/rtc.hpp
+++ Digital Clock/rtc.hpp
@@ -7,19 +7,55 @@
 class RTC
 {
 public:
-	void Initialize()
-	{
-		// TODO
-	}
-	
+	static const uint8_t i2c_address = 0b1101000;
+
 	void GetTime(DateTime& time)
 	{
-		// TODO
+		uint8_t seconds;
+		uint8_t minutes;
+		uint8_t hours;
+		uint8_t day_of_week;
+		uint8_t day;
+		uint8_t month;
+		uint8_t year;
+		
+		i2c.Start();
+		i2c.WriteAddress(i2c_address, true);
+		i2c.Write(0);
+		i2c.Start();
+		i2c.WriteAddress(i2c_address, false);
+		i2c.Read(seconds, true);
+		i2c.Read(minutes, true);
+		i2c.Read(hours, true);
+		i2c.Read(day_of_week, true);
+		i2c.Read(day, true);
+		i2c.Read(month, true);
+		i2c.Read(year, false);
+		i2c.Stop();
+		
+		time.second = from_bcd(seconds & 0x7f);
+		time.minute = from_bcd(minutes);
+		time.hour = from_bcd(hours & 0x3f);
+		time.day_of_week = day_of_week;
+		time.day = from_bcd(day);
+		time.month = from_bcd(month);
+		time.year = from_bcd(year);
 	}
 	
 	void SetTime(const DateTime& time)
 	{
-		// TODO
+		i2c.Start();
+		i2c.WriteAddress(i2c_address, true);
+		i2c.Write(0);
+		i2c.Write(time.second);
+		i2c.Write(time.minute);
+		i2c.Write(time.hour);
+		i2c.Write(time.day_of_week);
+		i2c.Write(time.day);
+		i2c.Write(time.month);
+		i2c.Write(time.year);
+		i2c.Write(0);
+		i2c.Stop();
 	}
 };
 
