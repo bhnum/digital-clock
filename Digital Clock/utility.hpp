6fa73f5994fb96f377053f5f0a0005af3ddffd7d
= <saedi.behnam.336@gmail.com>
2021-02-04 14:23:06 +0330

    Added utility functions

---
+++ Digital Clock/utility.hpp
@@ -0,0 +1,29 @@
+#pragma  once
+
+#include <stdint.h>
+
+// This class contains utility functions
+
+inline uint8_t modulo(int8_t a, uint8_t q)
+{
+	int8_t mod = a % q;
+	if (mod < 0)
+	mod += q;
+	return mod;
+}
+
+inline uint8_t posdiv(int8_t a, uint8_t q)
+{
+	return (a - modulo(a, q)) / q;
+}
+
+inline uint8_t to_bcd(uint8_t a)
+{
+	return ((a / 10) << 4) | (a % 10);
+}
+
+inline uint8_t from_bcd(uint8_t a)
+{
+	return (a >> 4) * 10 + (a & 0x0f);
+}
+
